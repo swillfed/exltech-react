2e41074d58cb050599121e15d9bff7040b30b7fc
swillfed <swilson@exltech.com.au>
2018-10-23 14:59:32 +1100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        // your wordpress source
+        baseUrl: `exltech.com.au`,
+        protocol: `https`,
+        // is it hosted on wordpress.com, or self-hosted?
+        hostingWPCOM: false,
+        // does your site use the Advanced Custom Fields Plugin?
+        useACF: false
+      }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import { rhythm } from "../utils/typography"
+
+class Home extends Component {
+  render() {
+    const data = this.props.data
+
+    return (
+      <Layout>
+        <div css={{ marginBottom: rhythm(1) }}>
+          <h1>Pages</h1>
+          {data.allWordpressPage.edges.map(({ node }) => (
+            <div key={node.slug}>
+              <Link to={node.slug} css={{ textDecoration: `none` }}>
+                <h3>{node.title}</h3>
+              </Link>
+              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+              <span>
+                {` `}
+                {node.date}
+              </span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <h1>Posts</h1>
+        {data.allWordpressPost.edges.map(({ node }) => (
+          <div css={{ marginBottom: rhythm(2) }} key={node.slug}>
+            <Link to={node.slug} css={{ textDecoration: `none` }}>
+              <h3>{node.title}</h3>
+            </Link>
+            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        ))}
+      </Layout>
+    )
+  }
+}
+
+export default Home
+
+// Set here the ID of the home page.
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
