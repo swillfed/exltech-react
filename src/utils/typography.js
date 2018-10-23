55971acec6c4790d19edea400cf9140b273ad2f1
swillfed <swilson@exltech.com.au>
2018-10-23 15:29:33 +1100

    added a console log

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,5 +3,7 @@ import grandViewTheme from "typography-theme-grand-view"
 
 const typography = new Typography(grandViewTheme)
 
+console.log(typography)
+
 export default typography
 export const rhythm = typography.rhythm
