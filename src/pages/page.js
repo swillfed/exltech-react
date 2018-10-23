ab902c77aed75a220222c0f6e14a06b6bf9ed9f1
swillfed <swilson@exltech.com.au>
2018-10-23 15:56:06 +1100

    Added markdown

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,14 +1,51 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
+import ReactMarkdown from 'react-markdown'
 
-const SecondPage = () => (
-  <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const markdown = `
+  # this is a title
+  ## This is a subtitle
+  [this](is a link)
+  this is some body test
+`
+const SecondPage = (props) => {
+  const data = props.data.allWordpressPage.edges.find(edge => edge.node.slug === props['*']).node
+  return (
+    <Layout>
+      <h1>{ data.title }</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.excerpt }} />
+      <ReactMarkdown source={ markdown } />
+      <br />
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+// Set here the ID of the home page.
+export const pageQuery = graphql`
+  query {
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          date(formatString: "MMMM DD, YYYY")
+        }
+      }
+    }
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
 
 export default SecondPage
